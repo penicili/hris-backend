b8e3ab2b12ef8751734156339e754961d0332ca2
Katon Bagaskoro <bagaskoro55@gmail.com>
2026-09-26 09:11:44 +0700

    feat: role base middleware

--- src/middlewares/authorize.middleware.ts
+++ src/middlewares/authorize.middleware.ts
@@ -1,1 +1,21 @@
 // Middleware buat RBAC
+import type { Request, Response, NextFunction } from "express"
+import type { Decrypted } from "../types/express"
+
+export const authorize = (allowedRoles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user as Decrypted;
+
+    if(!user){
+      return res.status(401)
+    }
+
+    if (!allowedRoles.includes(user.role)){
+      return res.status(403).json({
+        message: 'Forbidden'
+      })
+    }
+
+    next()
+  }
+}
